4afd93a2cfacaa4f130f30d83cd802cd14f34f33
trinhdaiphuc <trinhdaiphuc@gmail.com>
2025-12-02 13:40:52 +0700

    feat: Add 'fork' mode to PM2 configuration for improved process management.

--- pm2.config.js
+++ pm2.config.js
@@ -8,6 +8,7 @@ module.exports = {
 			autorestart: true,
 			watch: false,
 			max_memory_restart: "1G",
+			mode: "fork",
 			env: {
 				NODE_ENV: "production",
 				PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
